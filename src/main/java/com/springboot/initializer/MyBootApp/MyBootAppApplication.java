5d3c0e647fc087d851280889800afbf14399894c
Swapna <77243167+swapna2021@users.noreply.github.com>
2022-04-19 22:21:05 +0530

    commit

---
+++ src/main/java/com/springboot/initializer/MyBootApp/MyBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.initializer.MyBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppApplication.class, args);
+		System.out.println("hello ganesh");
+	}
+
+}
